40d7e29c0bc5fbab1e25e35f0ea6cea89d860cff
alish <alish.mirhashemi@gmail.com>
2018-10-23 23:38:19 +0330

    first commit

---
+++ src/FlightServiceBean.java
@@ -0,0 +1,19 @@
+import javax.ejb.LocalBean;
+import javax.ejb.Stateless;
+
+@Stateless(name = "FlightServiceEJB")
+@LocalBean
+public class FlightServiceBean {
+    public FlightServiceBean() {
+    }
+
+    private String name = "aaaaaaaaaa";
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
